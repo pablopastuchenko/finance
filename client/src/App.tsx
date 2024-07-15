6d44789d1cef0e08ed535a7e822be81485fa51f9
pastuchenko <pablopastuchenkooo@gmail.com>
2024-07-15 14:14:01 -0300

    first commit

---
+++ client/src/App.tsx
@@ -0,0 +1,20 @@
+import { createTheme } from "@mui/material/styles"
+import { CssBaseline, ThemeProvider } from "@mui/material"
+import { useMemo } from "react"
+import { themeSettings } from "./theme"
+
+
+
+function App() {
+  const theme = useMemo(()=>createTheme(themeSettings),[])
+  return (
+    <div className="app">
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        hi
+      </ThemeProvider>
+    </div>
+  )
+}
+
+export default App
